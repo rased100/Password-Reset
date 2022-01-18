75324ac2ec7dfcbde7ca18e87df08b27e7787ebc
rased100 <rased100@gmail.com>
2022-01-18 19:30:28 +0600

    finish

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -8,7 +8,6 @@ const Header = () => {
         <div>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand><Link className='link' to="/">Password Reset</Link></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
